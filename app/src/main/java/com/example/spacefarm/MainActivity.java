56adda53882c5118fe94e18a9a22fbe9910b503d
caleb turcotte <calebturcotte@cmail.carleton.ca>
2020-04-02 21:39:07 -0400

    initial memory changes

--- app/src/main/java/com/example/spacefarm/MainActivity.java
+++ app/src/main/java/com/example/spacefarm/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.spacefarm;
 
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,12 +13,18 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String PREFS_NAME = "MyPrefsFile";
+
     int money;
     TextView view;
     Farm farm1, farm2;
     boolean boughtfarm2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
@@ -27,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
         farm2 = new Farm(2);
         boughtfarm2 = false;
         view = findViewById(R.id.money);
-        money = 0;
+        money = settings.getInt("money", money);
+        view.setText(String.valueOf(money));
 
     }
 
@@ -36,16 +44,18 @@ public class MainActivity extends AppCompatActivity {
         if(view2.getId() == R.id.farm) {
             money += farm1.contains();
             farm1.reset();//resets any money that might be inside the farm
+            saveCash();//saves the money to shared preferences
         }
         else if(view2.getId() == R.id.farm2){
             if(boughtfarm2) {//checks if farm2 has been bought
                 money += farm2.contains();
                 farm2.reset();
+                saveCash();//saves the money to shared preferences
             }
             else if (money >= 20){
                 money = money - 20;
                 boughtfarm2 = true;
-
+                saveCash();//saves the money to shared preferences
             }
         }
         else if(view2.getId() == R.id.permafarm){
@@ -61,16 +71,28 @@ public class MainActivity extends AppCompatActivity {
             money += farm1.sell();
             money += farm2.sell();
             boughtfarm2 = false;
+            saveCash();//saves the money to shared preferences
         }
         else if(view2.getId() == R.id.upgrade){
             if(money >= 10){//buys an upgrade if there is enough money
                 money = money - 10;
                 farm1.upgrade();
                 farm2.upgrade();
+                saveCash();//saves the money to shared preferences
             }
+        }
+        else if(view2.getId() == R.id.reset){
+            money = 0;
+            boughtfarm2 = false;
         }
         view.setText(String.valueOf(money));
     }
 
+    public void saveCash(){
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("money",money);
+        editor.commit();
+    }
 
 }
